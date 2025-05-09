9369feb0ff1afdc430b619c6526d9a93eb88f17b
rai211 <rainikita211@gmail.com>
2025-05-09 14:21:33 +0530

    Feedback form added

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -80,7 +80,7 @@ const Feedback = () => {
         <div className="flex justify-center">
             <button
                 type="submit"
-                className="bg-blue-500 text-white px-40 py-2 rounded hover:bg-blue-600"
+                className="bg-blue-500 text-white px-20 py-2 rounded hover:bg-blue-600"
             >
                 Send
             </button>
